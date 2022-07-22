8db274430cc74faef260b71223fa19e244211d5d
JuanAnaya0730 <juan.anayar@udea.edu.co>
2022-07-21 20:11:48 -0500

    se implento una funcion que pasa un texto a su equivalente en binario

--- Lab3_Ejercicios_AnayaJuan/tools.cpp
+++ Lab3_Ejercicios_AnayaJuan/tools.cpp
@@ -34,3 +34,20 @@ void write(string name, string data)
         exit(1);
     }
 }
+
+string text_to_binary(string text)
+{
+    string binary, char_to_bin;
+
+    while(text.length()){
+        char_to_bin = "";
+        for(int i=0, ascii=int(text[0]); i<8; ascii /= 2, ++i){
+            char_to_bin += (ascii%2) + 48;
+        }
+        reverse(char_to_bin.begin(), char_to_bin.end());
+        binary += char_to_bin;
+        text = text.substr(1, text.length()-1);
+    }
+
+    return binary;
+}
