d168d49726ec793415d73345f190c6daa73b3a5c
JuanAnaya0730 <juan.anayar@udea.edu.co>
2022-07-21 19:07:48 -0500

    Se implemento la funcion que lee un archivo

--- Lab3_Ejercicios_AnayaJuan/tools.cpp
+++ Lab3_Ejercicios_AnayaJuan/tools.cpp
@@ -1,1 +1,21 @@
 #include "tools.h"
+
+string read(string name)
+{
+    ifstream file;
+    string data;
+
+    file.open(name);
+    if (file.is_open()){
+        while (!file.eof()) {
+            data += file.get();
+        }
+        file.close();
+    }
+    else{
+        cout << "Error al abrir el archivo." << endl;
+        exit(1);
+    }
+
+    return data;
+}
