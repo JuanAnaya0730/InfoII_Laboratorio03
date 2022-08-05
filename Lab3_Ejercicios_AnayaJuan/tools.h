645a1218b267400147eedf2e966c2515c7ed8a97
JuanAnaya0730 <juan.anayar@udea.edu.co>
2022-08-05 05:22:34 -0500

    se añadio el prototipo de la funcion que se encargara de encriptar usando el segundo metodo

--- Lab3_Ejercicios_AnayaJuan/tools.h
+++ Lab3_Ejercicios_AnayaJuan/tools.h
@@ -11,6 +11,7 @@ using namespace std;
 string read(string name);
 void write(string name, string data);
 string first_method(string data,int seed);
+string second_method(string data,int seed);
 string text_to_binary(string text);
 
 #endif // TOOLS_H
