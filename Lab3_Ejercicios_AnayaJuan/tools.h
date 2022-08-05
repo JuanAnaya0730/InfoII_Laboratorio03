e17c9f3acfc1caca7e657f9465bff37ca10132d6
JuanAnaya0730 <juan.anayar@udea.edu.co>
2022-08-05 05:45:07 -0500

    se añadio el prototipo de la funcion que se encargara de imprimir el menu principal, ademas de añadieron macros que contienen directivas de color, estos serviran para darle una mejor apariencia a la aplicacion

--- Lab3_Ejercicios_AnayaJuan/tools.h
+++ Lab3_Ejercicios_AnayaJuan/tools.h
@@ -6,8 +6,17 @@
 #include <fstream>
 #include <string>
 
+#define RESET "\e[0m"
+#define RED "\e[0;31m"
+#define GREEN "\e[0;32m"
+#define YELLOW "\e[0;33m"
+#define BLUE "\e[0;34m"
+#define MAGENTA "\e[0;35m"
+#define CYAN "\e[0;36m"
+
 using namespace std;
 
+int menu(void);
 string read(string name);
 void write(string name, string data);
 string first_method(string data,int seed);
