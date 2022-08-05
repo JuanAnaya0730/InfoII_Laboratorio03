4f0393feb794314fb0c0beaa6aa3229f6d9fbbb6
JuanAnaya0730 <juan.anayar@udea.edu.co>
2022-08-05 07:29:37 -0500

    se creo toda la interfaz del programa, la aplicacion como tal esta terminada en este punto

--- Lab3_Ejercicios_AnayaJuan/main.cpp
+++ Lab3_Ejercicios_AnayaJuan/main.cpp
@@ -1,9 +1,36 @@
-#include <iostream>
+#include "tools.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    system("color");
+    int option = menu();
+    string file_input, file_output;
+    int seed;
+
+    cout << "Ingrese el nombre del archivo a codificar\n -> ";
+    cin >> file_input;
+
+    system("cls");
+
+    cout << "Ingrese la semilla de codificacion\n ->  ";
+    cin >> seed;
+
+    system("cls");
+
+    cout << "Ingrese el nombre para el archivo codificado\n -> ";
+    cin >> file_output;
+
+    system("cls");
+
+    if(option == 1){
+        write(file_output, first_method(read(file_input), seed));
+    }else{
+        write(file_output, second_method(read(file_input), seed));
+    }
+
+    cout << CYAN "El archivo " << file_input << " has sido codificado con exito.\n" RESET << endl;
+
     return 0;
 }
