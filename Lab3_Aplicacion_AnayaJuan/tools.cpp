6182e95c7c03d3f1bfa0dbe207a207d6da07d1e5
JuanAnaya0730 <juan.anayar@udea.edu.co>
2022-08-11 23:13:26 -0500

    se copia la funcion de lectura de los ejercios, asi como las macros de los colores y las librerias que se usan en dichos ejercicios

--- Lab3_Aplicacion_AnayaJuan/tools.cpp
+++ Lab3_Aplicacion_AnayaJuan/tools.cpp
@@ -1,1 +1,33 @@
 #include "tools.h"
+
+string read(string name)
+{
+    // name es el nombre del archivo a leer
+
+    ifstream file; // Archivo a leer
+    string data; // Esto almacenara el contenido del archivo a leer
+    string aux; // Esto almacenara temporalmente cada linea del archivo a leer
+
+    file.open("../data/" + name); // Se abre el archivo
+    if (file.is_open()){ // Se verifica si el archivo abrio correctamente
+
+        /* Se recorre todo el archivo */
+        while (!file.eof()) {
+            getline(file, aux); // Se lee cada linea del archivo
+
+            /* Se guarda la linea del archivo y se le agrega un salto de linea ya que la
+             * funcion anterior los ignora*/
+            data += aux + '\n';
+        }
+
+        data.pop_back(); // Se elimina el ultimo salto de linea ya que este no pertenece al archivo
+
+        file.close(); // Se cierra el archivo
+    }
+    else{ // Si el archivo no se pudo abrir se muestra el mensaje correspondiente y finaliza el programa
+        cout << RED "Error al abrir el archivo " << name << RESET << endl;
+        exit(1);
+    }
+
+    return data; // Se retorna el contenido del archivo
+}
--- Lab3_Aplicacion_AnayaJuan/tools.h
+++ Lab3_Aplicacion_AnayaJuan/tools.h
@@ -1,4 +1,23 @@
 #ifndef TOOLS_H
 #define TOOLS_H
 
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <math.h>
+#include <string>
+
+#define RESET "\e[0m"
+#define RED "\e[0;31m"
+#define GREEN "\e[0;32m"
+#define YELLOW "\e[0;33m"
+#define BLUE "\e[0;34m"
+#define MAGENTA "\e[0;35m"
+#define CYAN "\e[0;36m"
+
+using namespace std;
+
+/*! Esta fucion es la encargada de leer un archivo */
+string read(string name);
+
 #endif // TOOLS_H
